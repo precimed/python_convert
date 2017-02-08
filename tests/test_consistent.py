9e3c9de9cba7cf850fd8cea15dacbca983a5a3e3
Oleksandr Frei <oleksandr.frei@gmail.com>
2017-02-08 18:12:18 +0100

    Fix unit-tests (second test failed because effect size column must be specified)

--- tests/test_consistent.py
+++ tests/test_consistent.py
@@ -9,11 +9,11 @@ def execute_command(command):
     print(process.communicate()[0].decode("utf-8"))
     #print(subprocess.check_output(command.split()).decode("utf-8"))
 
-def run(filename, matfile):
+def run(filename, matfile, effect):
     reffile = r'tests/1234_ref.bim'
     if os.path.isdir('TEMP_FOLDER'): shutil.rmtree('TEMP_FOLDER')
     execute_command(r'python sumstats_convert.py csv {} TEMP_FOLDER/TEST.csv --auto --force'.format(filename))
-    execute_command(r'python sumstats_convert.py mat {} TEMP_FOLDER/TEST.csv --traits test --force'.format(reffile))
+    execute_command(r'python sumstats_convert.py mat {} TEMP_FOLDER/TEST.csv --traits test --force --effect {}'.format(reffile, effect))
 
     f1 = sio.loadmat(matfile)
     f2 = sio.loadmat('TEMP_FOLDER/TEST.mat')
@@ -23,9 +23,9 @@ def run(filename, matfile):
     assert(max(abs(f1['zvec_test'] - f2['zvec_test'])) < 1e-10)
     shutil.rmtree('TEMP_FOLDER')
 
-def test01(): run('tests/case01.txt', 'tests/case01.mat')
-def test01gz(): run('tests/case01.txt.gz', 'tests/case01.mat')
+def test01(): run('tests/case01.txt', 'tests/case01.mat', effect='BETA')
+def test01gz(): run('tests/case01.txt.gz', 'tests/case01.mat', effect='BETA')
 #def test02(): run('tests/case02.txt')
 #def test03(): run('tests/case03.txt')
-def test04(): run('tests/case04.txt', 'tests/case04.mat')
-def test04gz(): run('tests/case04.txt.gz', 'tests/case04.mat')
+def test04(): run('tests/case04.txt', 'tests/case04.mat', effect='OR')
+def test04gz(): run('tests/case04.txt.gz', 'tests/case04.mat', effect='OR')
