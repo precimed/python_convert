6279b2f6cabcde336a6d7339de22595875e52ab0
Oleksandr Frei <oleksandr.frei@gmail.com>
2018-04-18 16:31:08 +0200

    sumstats.py clump - consistent results with FUMA

--- sumstats.py
+++ sumstats.py
@@ -16,12 +16,13 @@ import six
 from sumstats_utils import *
 import collections
 import re
-from shutil import copyfile
+from shutil import copyfile, rmtree
 import zipfile
 import glob
 import socket
 import getpass
 import subprocess
+import tarfile
 
 __version__ = '1.0.0'
 MASTHEAD = "***********************************************************************\n"
@@ -171,35 +172,39 @@ def parse_args(args):
 
     # 'clump' utility: clump summary stats, produce lead SNP report, produce candidate SNP report
     parser_clump = subparsers.add_parser("clump",
-        help="""Perform LD-based clumping of summary stats. This works as follows.
+        help="""Perform LD-based clumping of summary stats. This works similar to FUMA snp2gene functionality (http://fuma.ctglab.nl/tutorial#snp2gene).
     Step 1. Re-save summary stats into one file for each chromosome.
-    Step 2. Use 'plink --clump' to find clumps and lead SNPs
-    Step 3. Use 'plink --ld' to find genomic loci around each lead
+    Step 2a Use 'plink --clump' to find independent significant SNPs (default r2=0.6)
+    Step 2b Use 'plink --clump' to find lead SNPs, by clumping independent significant SNPs (default r2=0.1)
+    Step 3. Use 'plink --ld' to find genomic loci around each independent significant SNP (default r2=0.6)
     Step 4. Merge together genomic loci which are closer than certain threshold (250 KB)
     Step 5. Merge together genomic loci that fall into exclusion regions, such as MHC
     Step 6. Output genomic loci report, indicating lead SNPs for each loci
     Step 7. Output candidate SNP report""")
-    parser_clump.add_argument("--sumstats", type=str, help="[required] Input file with summary statistics")
+    parser_clump.add_argument("--sumstats", type=str, help="Input file with summary statistics")
     parser_clump.add_argument("--out", type=str, help="[required] File to output the result.")
     parser_clump.add_argument("--force", action="store_true", default=False, help="Allow sumstats.py to overwrite output file if it exists.")
+    parser_clump.add_argument("--chr-labels", type=str, nargs='+',
+        help="List of chromosome labels to substitute for @, default to 1..22")
 
-    parser_clump.add_argument("--clump-field", type=str, default='PVAL', help="Column to clump on. For details see plink documentation.")
-    parser_clump.add_argument("--clump-snp-field", type=str, default='SNP', help="Column with marker name. For details see plink documentation.")
+    parser_clump.add_argument("--clump-field", type=str, default='PVAL', help="Column to clump on.")
+    parser_clump.add_argument("--clump-snp-field", type=str, default='SNP', help="Column with marker name.")
     parser_clump.add_argument("--chr", type=str, default='CHR', help="Column name with chromosome labels. ")
-    parser_clump.add_argument("--clump-r2", type=float, default=0.1, help="LD r2 threshold for clumping lead SNPs. For details see plink documentation. ")
-    parser_clump.add_argument("--clump-p1", type=float, default=5e-8, help="lead variant p-value threshold. For details see plink documentation. ")
+
+    parser_clump.add_argument("--indep-r2", type=float, default=0.6, help="LD r2 threshold for clumping independent significant SNPs.")
+    parser_clump.add_argument("--lead-r2", type=float, default=0.1, help="LD r2 threshold for clumping lead SNPs.")
+    parser_clump.add_argument("--clump-p1", type=float, default=5e-8, help="p-value threshold for independent significant SNPs.")
     parser_clump.add_argument("--bfile-chr", type=str,
         help="prefix for plink .bed/.bim/.fam file. Will automatically concatenate .bed/.bim/.fam files split across 22 chromosomes. "
         "If the filename prefix contains the symbol @, sumstats.py will replace the @ symbol with chromosome numbers. "
         "Otherwise, sumstats.py will append chromosome numbers to the end of the filename prefix. ")
     parser_clump.add_argument("--ld-window-kb", type=float, default=10000, help="Window size in KB to search for clumped SNPs. ")
-    parser_clump.add_argument("--ld-window-r2", type=float, default=0.6, help="LD r2 threshold to search for clumped SNPs. ")
     parser_clump.add_argument("--loci-merge-kb", type=float, default=250, help="Maximum distance in KB of LD blocks to merge. ")
     parser_clump.add_argument("--exclude-ranges", type=str, nargs='+',
         help='Exclude SNPs in ranges of base pair position, for example MHC. '
         'The syntax is chr:from-to, for example 6:25000000-35000000. Multiple regions can be excluded.')
     parser_clump.add_argument("--plink", type=str, default='plink', help="Path to plink executable.")
-
+    parser_clump.add_argument("--sumstats-chr", type=str, help="Input file with summary statistics, one file per chromosome")
     parser_clump.set_defaults(func=make_clump)
 
     # 'rs' utility: augument summary statistic file with SNP RS number from reference file
@@ -965,70 +970,170 @@ def make_clump(args, log):
     TBD: refine output tables
     TBD: in snps table, do an outer merge - e.i, include SNPs that pass p-value threshold (some without locus number), and SNPs without p-value (e.i. from reference genotypes)
     """
-    check_input_file(args.sumstats)
     #check_output_file(args.out, args.force)
     #for chri in range(1, 23):
     #    check_input_file(sub_chr(args.bfile_chr, chri) + '.bed')
     exclude_ranges = make_ranges(args.exclude_ranges, log)
 
-    log.log('Reading {}...'.format(args.sumstats))
-    df_sumstats = pd.read_table(args.sumstats, delim_whitespace=True)
-    log.log('Read {} SNPs from --sumstats file'.format(len(df_sumstats)))
+    temp_out = args.out + '.temp'
+    if not os.path.exists(temp_out):
+        os.makedirs(temp_out)
 
-    for cname in [args.clump_field, args.clump_snp_field, args.chr]:
-        if cname not in df_sumstats.columns:
-            raise ValueError('{} column not found in {}; available columns: '.format(cname, args.sumstats, df_sumstats.columns))
+    if (not args.sumstats) and (not args.sumstats_chr):
+        raise ValueError('At least one of --sumstats or --sumstats-chr must be specified')
 
-    for chri in range(1, 23):
-        if not args.bfile_chr:
-            break
+    if args.chr_labels is None:
+        args.chr_labels = list(range(1, 23))
 
-        df_sumstats[df_sumstats[args.chr] == chri].to_csv('{}.sumstats.chr{}.csv'.format(args.out, chri),sep='\t',index=False)
+    if args.sumstats_chr:
+        args.sumstats_chr = [sub_chr(args.sumstats_chr, chri) for chri in args.chr_labels]
+    else:
+        args.sumstats_chr = ['{}/sumstats.chr{}.csv'.format(temp_out, chri) for chri in args.chr_labels]
+
+    def validate_columns(df):
+        for cname in [args.clump_field, args.clump_snp_field, args.chr]:
+            if cname not in df.columns:
+                raise ValueError('{} column not found in {}; available columns: '.format(cname, args.sumstats, df.columns))
+
+    if args.sumstats:
+        check_input_file(args.sumstats)
+        log.log('Reading {}...'.format(args.sumstats))
+        df_sumstats = pd.read_table(args.sumstats, delim_whitespace=True)
+        log.log('Read {} SNPs from --sumstats file'.format(len(df_sumstats)))
+        validate_columns(df_sumstats)
+        for chri, df_chr_file in zip(args.chr_labels, args.sumstats_chr):
+            df_sumstats[df_sumstats[args.chr] == chri].to_csv(df_chr_file, sep='\t',index=False)
+    else:
+        for df_chr_file in args.sumstats_chr:
+            check_input_file(df_chr_file)
+        log.log('Reading {}...'.format(args.sumstats_chr))
+        df_sumstats = pd.concat([pd.read_table(df_chr_file, delim_whitespace=True) for df_chr_file in args.sumstats_chr])
+        log.log('Read {} SNPs'.format(len(df_sumstats)))
+
+    for chri, df_chr_file in zip(reversed(args.chr_labels), reversed(args.sumstats_chr)):
+        # Step1 - find independent significant SNPs
         execute_command(
             "{} ".format(args.plink) +
             "--bfile {} ".format(sub_chr(args.bfile_chr, chri)) +
-            "--clump {}.sumstats.chr{}.csv ".format(args.out, chri) +
-            "--clump-p1 {} --clump-p2 1 ".format(args.clump_p1) + 
-            "--clump-r2 {} --clump-kb 1e9 ".format(args.clump_r2) +
+            "--clump {} ".format(df_chr_file) +
+            "--clump-p1 {} --clump-p2 1 ".format(args.clump_p1) +
+            "--clump-r2 {} --clump-kb 1e9 ".format(args.indep_r2) +
             "--clump-snp-field {} --clump-field {} ".format(args.clump_snp_field, args.clump_field) +
-            "--out {}.chr{} ".format(args.out, chri),
+            "--out {}/indep.chr{} ".format(temp_out, chri),
             log)
-        if not os.path.isfile('{}.chr{}.clumped'.format(args.out, chri)): continue
-        pd.read_table('{}.chr{}.clumped'.format(args.out, chri), delim_whitespace=True)['SNP'].to_csv('{}.chr{}.clumped.snps'.format(args.out, chri), index=False, header=False)
+
+        if not os.path.isfile('{}/indep.chr{}.clumped'.format(temp_out, chri)):
+            log.log('On CHR {} no variants pass significance threshold'.format(chri)) 
+            continue
+
+        # Step 2 - find lead SNPs by clumping together independent significant SNPs
+        execute_command(
+            "{} ".format(args.plink) +
+            "--bfile {} ".format(sub_chr(args.bfile_chr, chri)) +
+            "--clump {} ".format('{}/indep.chr{}.clumped'.format(temp_out, chri)) +
+            "--clump-p1 {} --clump-p2 1 ".format(args.clump_p1) +
+            "--clump-r2 {} --clump-kb 1e9 ".format(args.lead_r2) +
+            "--clump-snp-field SNP --clump-field P " +
+            "--out {}/lead.chr{} ".format(temp_out, chri),
+            log)
+
+        # Step 3 - find loci around independent significant SNPs
+        pd.read_table('{}/indep.chr{}.clumped'.format(temp_out, chri), delim_whitespace=True)['SNP'].to_csv('{}/indep.chr{}.clumped.snps'.format(temp_out, chri), index=False, header=False)
         execute_command(
             "{} ".format(args.plink) + 
             "--bfile {} ".format(sub_chr(args.bfile_chr, chri)) +
-            "--r2 --ld-window {} --ld-window-r2 {} ".format(args.ld_window_kb, args.ld_window_r2) +
-            "--ld-snp-list {out}.chr{chri}.clumped.snps ".format(out=args.out, chri=chri) +
-            "--out  {out}.chr{chri} ".format(out=args.out, chri=chri),
+            "--r2 --ld-window {} --ld-window-r2 {} ".format(args.ld_window_kb, args.indep_r2) +
+            "--ld-snp-list {out}/indep.chr{chri}.clumped.snps ".format(out=temp_out, chri=chri) +
+            "--out  {out}/indep.chr{chri} ".format(out=temp_out, chri=chri),
             log)
 
-    # group loci together
-    files = ["{out}.chr{chri}.ld".format(out=args.out, chri=chri) for chri in range(1, 23)]
-    df_cand=pd.concat([pd.read_table(file, delim_whitespace=True) for file in files if os.path.isfile(file)])
-    df_sumstats.drop_duplicates(subset=args.clump_snp_field, inplace=True)
-    df_cand = pd.merge(df_cand, df_sumstats, how='left', left_on='SNP_B', right_on=args.clump_snp_field)
-    df_cand.to_csv('{}.snps.csv'.format(args.out), sep='\t', index=False)
-    df_loci=df_cand.groupby(['SNP_A', 'CHR_A']).agg({'BP_B':['min', 'max']})
-    df_loci.reset_index(inplace=True)
-    df_loci.columns=['SNP_A', 'CHR_A', 'MinBP', 'MaxBP']
-    df_loci=df_loci.sort_values(['CHR_A', 'MinBP']).reset_index(drop=True)
-    df_loci['locusnum'] = df_loci.index + 1
-    df_loci = pd.merge(df_loci, df_sumstats[[args.clump_snp_field, args.clump_field]], how='left', left_on='SNP_A', right_on=args.clump_snp_field)
-    while True:
-        df_loci['MaxBPprev'] = [(df_loci['MaxBP'][i-1] if ((i > 0) and (df_loci['CHR_A'][i] == df_loci['CHR_A'][i-1]))  else np.nan) for i in range(len(df_loci))] 
-        df_loci['dist2prev'] = df_loci['MinBP'] - df_loci['MaxBPprev']
-        df_loci['locusnum2'] = [ ( df_loci['locusnum'][i-1] if (df_loci['dist2prev'][i] < (1000 * args.loci_merge_kb)) else df_loci['locusnum'][i] ) for i in range(len(df_loci))]
-        for exrange in exclude_ranges:
-            df_loci['locusnum2'] = [ ( df_loci['locusnum2'][i-1] if ((df_loci['CHR_A'][i] == exrange.chr) and (df_loci['MaxBPprev'][i] >= exrange.from_bp) and (df_loci['MinBP'][i] <= exrange.to_bp)) else df_loci['locusnum2'][i] ) for i in range(len(df_loci))]
-        if (df_loci['locusnum2'] == df_loci['locusnum']).all():
-            df_loci.drop(['locusnum2', 'dist2prev', 'MaxBPprev'], axis=1, inplace=True)
-            break
-        df_loci['locusnum'] = df_loci['locusnum2']
+    # find indep to lead SNP mapping (a data frame with columns 'LEAD' and 'INDEP')
+    files = ["{}/lead.chr{}.clumped".format(temp_out, chri) for chri in args.chr_labels]
+    files = [file for file in files if os.path.isfile(file)]
+    if not files: raise ValueError('No .clumped files found - could it be that no variants pass significance threshold?')
+    lead_to_indep = []
+    for file in files:
+        df=pd.read_table(file,delim_whitespace=True)
+        lead_to_indep.append(pd.concat([pd.DataFrame(data=[(lead, indep_snp.split('(')[0]) for indep_snp in indep_snps.split(',') if indep_snp != 'NONE'] + [(lead, lead)], columns=['LEAD', 'INDEP']) for lead, indep_snps in zip(df['SNP'].values, df['SP2'].values)]))
+    lead_to_indep = pd.concat(lead_to_indep).reset_index(drop=True)
+    if lead_to_indep.duplicated(subset=['INDEP'], keep=False).any():
+        raise ValueError('Some independent significant SNP belongs to to lead SNPs; this is an internal error in sumstats.py logic - please report this bug.')
+    log.log('{} independent significant SNPs, {} lead SNPs'.format(len(set(lead_to_indep['INDEP'])), len(set(lead_to_indep['LEAD']))))
 
-    df_loci['locusnum'] = df_loci['locusnum'].map({l:i+1 for (i, l) in enumerate(df_loci['locusnum'].unique())})
-    df_loci['is_lead'] = (df_loci[args.clump_field] == df_loci.groupby(['locusnum'])[args.clump_field].transform(min))
-    df_loci.to_csv('{}.loci.csv'.format(args.out), sep='\t', index=False)
+    # group loci together:
+    # SNP_A is an indepependent significant SNP
+    # SNP_B is a candidate SNP
+    # LEAD_SNP is lead SNP
+    # R2 is always correlation between candidate and independent significant SNP
+    files = ["{out}/indep.chr{chri}.ld".format(out=temp_out, chri=chri) for chri in args.chr_labels]
+    files = [file for file in files if os.path.isfile(file)]
+    if not files: raise ValueError('No .ld files found')
+    df_cand=pd.concat([pd.read_table(file, delim_whitespace=True) for file in files])
+    df_cand=pd.merge(df_cand, lead_to_indep.rename(columns={'INDEP':'SNP_A', 'LEAD':'LEAD_SNP'}), how='left', on='SNP_A')
+    df_sumstats.drop_duplicates(subset=args.clump_snp_field, inplace=True)
+    df_cand = pd.merge(df_cand, df_sumstats.rename(columns={args.clump_snp_field: 'SNP_B'}), how='left', on='SNP_B')
+    df_lead=df_cand.groupby(['LEAD_SNP', 'CHR_A']).agg({'BP_B':['min', 'max']})
+    df_lead.reset_index(inplace=True)
+    df_lead.columns=['LEAD_SNP', 'CHR_A', 'MinBP', 'MaxBP']
+    df_lead=df_lead.sort_values(['CHR_A', 'MinBP']).reset_index(drop=True)
+    df_lead['locusnum'] = df_lead.index + 1
+    df_lead = pd.merge(df_lead, df_sumstats[[args.clump_snp_field, args.clump_field]].rename(columns={args.clump_snp_field: 'LEAD_SNP'}), how='left', on='LEAD_SNP')
+    df_lead['MaxBP_locus'] = df_lead['MaxBP']
+    while True:
+        has_changes = False
+        for i in range(1, len(df_lead)):
+            if df_lead['CHR_A'][i] != df_lead['CHR_A'][i-1]: continue
+
+            merge_to_previous = False
+            if (df_lead['MinBP'][i] - df_lead['MaxBP_locus'][i-1]) < (1000 * 250):
+                merge_to_previous = True
+            for exrange in exclude_ranges:
+                if df_lead['CHR_A'][i] != exrange.chr: continue
+                if (df_lead['MaxBP_locus'][i-1] >= exrange.from_bp) and (df_lead['MinBP'][i] <= exrange.to_bp):
+                    merge_to_previous = True
+            if merge_to_previous and (df_lead['locusnum'][i] != df_lead['locusnum'][i-1]):
+                log.log('Merge locus {} to {}'.format(df_lead['locusnum'][i], df_lead['locusnum'][i-1]))
+                df_lead['locusnum'][i] = df_lead['locusnum'][i-1]
+                df_lead['MaxBP_locus'][i] = np.max([df_lead['MaxBP_locus'][i-1], df_lead['MaxBP_locus'][i]])
+                has_changes = True
+                break
+
+        if not has_changes:
+            df_lead.drop(['MaxBP_locus'], axis=1, inplace=True)
+            break  # exit from "while True:" loop
+
+    df_lead['locusnum'] = df_lead['locusnum'].map({l:i+1 for (i, l) in enumerate(df_lead['locusnum'].unique())})
+    df_lead['is_locus_lead'] = (df_lead[args.clump_field] == df_lead.groupby(['locusnum'])[args.clump_field].transform(min))
+    df_lead = pd.merge(df_lead, df_cand[['SNP_B', 'BP_B']].drop_duplicates().rename(columns={'SNP_B':'LEAD_SNP', 'BP_B':'LEAD_BP'}), how='left', on='LEAD_SNP')
+    cols = list(df_lead)
+    cols.insert(0, cols.pop(cols.index('LEAD_BP')))
+    cols.insert(0, cols.pop(cols.index('LEAD_SNP')))
+    cols.insert(0, cols.pop(cols.index('CHR_A')))
+    cols.insert(0, cols.pop(cols.index('locusnum')))
+    df_lead[cols].rename(columns={'CHR_A':'CHR'}).to_csv('{}.lead.csv'.format(args.out), sep='\t', index=False)
+    log.log('{} lead SNPs reported to {}.lead.csv'.format(len(df_lead), args.out))
+
+    df_loci=df_lead.groupby(['locusnum']).agg({'MinBP':'min', 'MaxBP':'max', 'CHR_A':'min', 'PVAL':'min'})
+    df_loci.reset_index(inplace=True)
+    df_loci=pd.merge(df_loci, df_lead[df_lead['is_locus_lead'].values][['locusnum', 'LEAD_SNP', 'LEAD_BP']], on='locusnum', how='left')
+    df_loci.rename(columns={'CHR_A':'CHR'})[['locusnum', 'CHR', 'LEAD_SNP', 'LEAD_BP', 'MinBP', 'MaxBP', 'PVAL']].to_csv('{}.loci.csv'.format(args.out), sep='\t', index=False)
+    log.log('{} loci reported to {}.loci.csv'.format(len(df_loci), args.out))
+
+    df_cand = pd.merge(df_cand, df_lead[['LEAD_SNP', 'locusnum']], how='left', on='LEAD_SNP')
+    cols = list(df_cand); cols.insert(0, cols.pop(cols.index('locusnum')))
+    df_cand = df_cand[cols].drop(['CHR_B'], axis=1).rename(columns={'CHR_A':'CHR', 'BP_A':'INDEP_BP', 'SNP_A':'INDEP_SNP', 'BP_B':'CAND_BP', 'SNP_B':'CAND_SNP'}).copy()
+    df_cand.to_csv('{}.snps.csv'.format(args.out), sep='\t', index=False)
+    log.log('{} candidate SNPs reported to {}.snps.csv'.format(len(df_cand), args.out))
+
+    df_indep=df_cand[df_cand['CAND_SNP'] == df_cand['INDEP_SNP']].copy()
+    df_indep.drop(['CAND_BP','CAND_SNP'], axis=1, inplace=True)
+    df_indep.to_csv('{}.indep.csv'.format(args.out), sep='\t', index=False)
+    log.log('{} independent significant SNPs reported to {}.snps.csv'.format(len(df_indep), args.out))
+
+    log.log('Saving intermediate files to {out}.temp.tar.gz'.format(out=args.out))
+    with tarfile.open('{out}.temp.tar.gz'.format(out=args.out), "w:gz") as tar:
+        tar.add(temp_out, arcname=os.path.basename(temp_out), exclude=lambda filename : os.path.basename(filename).startswith('sumstats'))
+    rmtree(temp_out)
 
 ### =================================================================================
 ###                          Implementation for parser_rs
